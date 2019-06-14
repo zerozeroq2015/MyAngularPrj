b843f0ee9a743ee04d7324697015a0913a8a7468
zerozeroq2015 <zerozeroq@gmail.com>
2019-06-14 06:14:26 +0000

    2 files update

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -10,7 +10,7 @@ export class AppService {
   
   getMessage(callback: (msg: String) => void): void {
     // API GatewayとHTTP通信して、取得成功時にコールバックを行なう
-    this.http.get('https://dxfl0gwu0m.execute-api.us-east-2.amazonaws.com/Angular01Stage', {  // API GatewayのURL
+    this.http.get('https://dxfl0gwu0m.execute-api.us-east-2.amazonaws.com/Angular01Stage/helloangular', {  // API GatewayのURL
       headers: new HttpHeaders().set('x-api-key', 'FdTcSTUnaw4Hq2z54kb9h1nBUbVSGsXl1SyZYXnO') // APIキー文字列
     }).subscribe(data => {
       callback(data['name']);
