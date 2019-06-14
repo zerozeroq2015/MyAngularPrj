cff24403eb278e46c0f46a42e34226dc008f512a
zerozeroq2015 <zerozeroq@gmail.com>
2019-06-14 04:25:34 +0000

    AppService.getMessageの追加

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { AppService } from './app.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers:[ AppService ]
 })
 export class AppComponent {
+  
+  constructor(private appService: AppService){}
+  
   title = 'MyAngularPrj';
+  
+  ngOnInit(): void {
+    this.appService.getMessage(msg => {
+      this.title == msg;  
+    })
+  }
 }
