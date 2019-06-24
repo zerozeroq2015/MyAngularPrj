87ae112ea1b7b08e7241f7b303548af50b1308e2
zerozeroq2015 <46638819+zerozeroq2015@users.noreply.github.com>
2019-06-24 15:05:00 +0900

    Test

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+
+const body = {emp_id: 10000};
+const httpOptions = {
+  withCredentials: true
+};
+httpOptions['headers'] = new HttpHeaders({
+  'Content-Type': 'application/x-www-form-urlencoded',
+});
 
 export interface Name {
-  name: string;
+  emp_name: string;
 }
 
 @Injectable({
@@ -15,10 +23,19 @@ export class AppService {
   constructor(private http: HttpClient) { }
   
   getMessage(callback: (msg: String) => void): void {
-    this.http.get(this.url).subscribe((data: Name) => {
-      callback(data['name']);
+    // this.http.get(this.url).subscribe((data: Name) => {
+    //   callback(data['name']);
+    let body = new HttpParams({
+        fromObject: {
+        'emp_id': '10000'
+        }
     });
-
+    this.http.post<any>(this.url, body, httpOptions);  
+    callback('callback dekita');
+    // this.http.post(this.url, body).subscribe((data: Name) => {
+    //     callback(data['emp_name']);
+    //   }
+    // );
     // // API GatewayとHTTP通信して、取得成功時にコールバックを行なう
     // this.http.get('https://dxfl0gwu0m.execute-api.us-east-2.amazonaws.com/Angular01Stage/helloangular', {  // API GatewayのURL
     //   headers: new HttpHeaders().set('x-api-key', 'FdTcSTUnaw4Hq2z54kb9h1nBUbVSGsXl1SyZYXnO') // APIキー文字列
