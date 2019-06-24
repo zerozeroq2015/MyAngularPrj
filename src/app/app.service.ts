7fdc9882c5f656525d64962a765801806d9d9536
zerozeroq2015 <46638819+zerozeroq2015@users.noreply.github.com>
2019-06-24 17:57:55 +0900

    hukugen

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -18,25 +18,13 @@ export interface Name {
 })
 export class AppService {
    
-  // url = 'https://dxfl0gwu0m.execute-api.us-east-2.amazonaws.com/Angular01Stage/helloangular';
-  url = 'https://dxfl0gwu0m.execute-api.us-east-2.amazonaws.com/Angular01Stage/selectangular';
+  url = 'https://dxfl0gwu0m.execute-api.us-east-2.amazonaws.com/Angular01Stage/helloangular';
 
   constructor(private http: HttpClient) { }
   
   getMessage(callback: (msg: String) => void): void {
-    // this.http.get(this.url).subscribe((data: Name) => {
-    //   callback(data['name']);
-    let body = new HttpParams({
-        fromObject: {
-        'emp_id': '10000'
-        }
-    });
-    this.http.post<any>(this.url, body, httpOptions);  
-    callback('callback dekita');
-    // this.http.post(this.url, body).subscribe((data: Name) => {
-    //     callback(data['emp_name']);
-    //   }
-    // );
+    this.http.get(this.url).subscribe((data: Name) => {
+      callback(data['name']);
     // // API GatewayとHTTP通信して、取得成功時にコールバックを行なう
     // this.http.get('https://dxfl0gwu0m.execute-api.us-east-2.amazonaws.com/Angular01Stage/helloangular', {  // API GatewayのURL
     //   headers: new HttpHeaders().set('x-api-key', 'FdTcSTUnaw4Hq2z54kb9h1nBUbVSGsXl1SyZYXnO') // APIキー文字列
